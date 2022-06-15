62a09ef56a8c44b4085225e951afc8c01c41dc9c
Negin Shariat <negin_shariat@hotmail.com>
2022-06-15 15:07:02 +0200

    first commit

---
+++ src/model/Customer.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Customer {
+    String firstName;
+    String lastName;
+    String email;
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/model/Room.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Room implements IRoom{
+    @Override
+    public String getRoomNumber() {
+        return null;
+    }
+
+    @Override
+    public Double getRoomPrice() {
+        return null;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return null;
+    }
+
+    @Override
+    public boolean isFree() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{}";
+    }
+}
