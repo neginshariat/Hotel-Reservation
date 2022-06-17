2306fe6578cd80536ae6cf164cd3a971c4444be2
Negin Shariat <negin_shariat@hotmail.com>
2022-06-17 15:06:35 +0200

    service bug

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -6,6 +6,23 @@ public class Customer {
     private String firstName;
     private String lastName;
     private String email;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     private final String emailREX= "^(.+)@(.+).(.+)$";
     private final Pattern pattern= Pattern.compile(emailREX);
     public Customer(String firstName, String lastName, String email){
---
+++ src/model/Driver.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Driver {
+    public static void main(String[] args){
+        Customer customer = new Customer("Negin","Shariat","negin_shariat@hotmail.com");
+        System.out.println(customer);
+    }
+}
