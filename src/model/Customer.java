021a543927da2a51f5a5d5002c9de32e4053396c
Negin Shariat <negin_shariat@hotmail.com>
2022-06-17 22:00:55 +0200

    edit customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -15,6 +15,14 @@ public class Customer {
         this.lastName = lastName;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getEmail() {
         return email;
     }
