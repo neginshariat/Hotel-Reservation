ca123b103db90f3b52d28887ea55b2d229c265d4
Negin Shariat <negin_shariat@hotmail.com>
2022-06-17 15:10:02 +0200

    service bug

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,2 +1,20 @@
-package service;public class CustomerService {
+package service;
+
+import model.Customer;
+
+import java.util.Collection;
+
+public class CustomerService {
+    private Customer customer;
+    private String firstName;
+    private String lastName;
+    public void addCustomer(String firstName, String lastName, String email){
+        Customer newCustomer = new Customer(firstName,lastName,email);
+    }
+    public Customer getCustomer(String customerEmail){
+        return customer;
+    }
+    public Collection<Customer> getAllCustomer(){
+        Customer modelCustomer= new Customer();
+    }
 }
